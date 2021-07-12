14181871493ae1ce724732fdeafb64b4ccc5ee38
Philipp Normann <philipp.normann@gmail.com>
2021-07-12 22:46:59 +0200

    Add a config flag to disable notifications entirely Fixes: https://github.com/philippnormann/chia-monitor/issues/14

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A monitoring tool to collect all important metrics from your Chia farming node a
 This example dashboard displays almost all collected metrics and can be imported from [grafana.com](https://grafana.com/grafana/dashboards/14544) using the ID `14544` or using the `grafana/dashboard.json` from this repository.
 
 ## Notifications
-To use notifications, please configure a `status_service_url` and `alert_service_url` for your desired notification service in the `config.json`. You can use most popular notifications services by creating a service specific webhook URL, following the instructions from [this](https://github.com/caronc/apprise/wiki) wiki.
+To use notifications, please configure a `status_service_url` and `alert_service_url` for your desired notification service in the `config.json`. You can use most popular notifications services by creating a service specific webhook URL, following the instructions from [this](https://github.com/caronc/apprise/wiki) wiki. If you wish to disable notifications entirely, you can set the `enable` field in the `notifications` section of the `config.json` to `false`. 
 
 ---
 Following notifications are currently sent to the `status_service_url`:
--- monitor/__main__.py
+++ monitor/__main__.py
@@ -3,6 +3,7 @@ import json
 import logging
 import sys
 from asyncio.queues import Queue
+from typing import Optional
 
 import colorlog
 from chia.util.config import load_config
@@ -36,7 +37,7 @@ async def persist_event(event: ChiaEvent):
         await db_session.commit()
 
 
-async def aggregator(exporter: ChiaExporter, notifier: Notifier) -> None:
+async def aggregator(exporter: ChiaExporter, notifier: Optional[Notifier]) -> None:
     rpc_collector = None
     ws_collector = None
     event_queue = Queue()
@@ -57,7 +58,8 @@ async def aggregator(exporter: ChiaExporter, notifier: Notifier) -> None:
         logging.info("🚀 Starting monitoring loop!")
         asyncio.create_task(rpc_collector.task())
         asyncio.create_task(ws_collector.task())
-        asyncio.create_task(notifier.task())
+        if notifier is not None:
+            asyncio.create_task(notifier.task())
         while True:
             try:
                 event = await event_queue.get()
@@ -93,20 +95,24 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
+        enable_notifications = config["notifications"]["enable"]
         status_url = config["notifications"]["status_service_url"]
         alert_url = config["notifications"]["alert_service_url"]
         status_interval_minutes = config["notifications"]["status_interval_minutes"]
         lost_plots_alert_threshold = config["notifications"]["lost_plots_alert_threshold"]
         disable_proof_found_alert = config["notifications"]["disable_proof_found_alert"]
-    except KeyError:
+    except KeyError as ex:
         logging.error(
-            "Failed to validate config. Please compare the fields of your config.json with the config-example.json and fix all inconsistencies."
+            f"Failed to validate config. Missing required key {ex}. Please compare the fields of your config.json with the config-example.json and fix all inconsistencies."
         )
         sys.exit(1)
 
-    notifier = Notifier(status_url, alert_url, status_interval_minutes, lost_plots_alert_threshold,
-                        disable_proof_found_alert)
     exporter = ChiaExporter()
+    if enable_notifications:
+        notifier = Notifier(status_url, alert_url, status_interval_minutes, lost_plots_alert_threshold,
+                            disable_proof_found_alert)
+    else:
+        notifier = None
 
     try:
         asyncio.run(aggregator(exporter, notifier))
