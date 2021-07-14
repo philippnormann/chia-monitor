afbd24f580547bc4335e6c9aef966d7bdbd34715
Philipp Normann <philipp.normann@gmail.com>
2021-07-15 01:20:31 +0200

    Remove unique constraint on ts for farming events This prevents collisions for simultaneous farming events as described in https://github.com/philippnormann/chia-monitor/issues/15

--- monitor/database/events.py
+++ monitor/database/events.py
@@ -5,7 +5,7 @@ from monitor.database import ChiaEvent
 
 class HarvesterPlotsEvent(ChiaEvent):
     __tablename__ = "harvester_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     host = Column(String(255), primary_key=True)
     plot_count = Column(Integer)
     portable_plot_count = Column(Integer)
@@ -15,7 +15,7 @@ class HarvesterPlotsEvent(ChiaEvent):
 
 class ConnectionsEvent(ChiaEvent):
     __tablename__ = "connection_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     full_node_count = Column(Integer)
     farmer_count = Column(Integer)
     wallet_count = Column(Integer)
@@ -24,7 +24,7 @@ class ConnectionsEvent(ChiaEvent):
 
 class BlockchainStateEvent(ChiaEvent):
     __tablename__ = "blockchain_state_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     space = Column(String(32))
     diffculty = Column(Integer)
     peak_height = Column(String(32))
@@ -33,14 +33,14 @@ class BlockchainStateEvent(ChiaEvent):
 
 class WalletBalanceEvent(ChiaEvent):
     __tablename__ = "wallet_balance_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     confirmed = Column(String(32))
     farmed = Column(String(32))
 
 
 class SignagePointEvent(ChiaEvent):
     __tablename__ = "signage_point_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     challenge_hash = Column(String(66), index=True)
     signage_point = Column(String(66), index=True)
     signage_point_index = Column(Integer)
@@ -48,7 +48,8 @@ class SignagePointEvent(ChiaEvent):
 
 class FarmingInfoEvent(ChiaEvent):
     __tablename__ = "farming_info_events"
-    ts: float = Column(DateTime, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ts = Column(DateTime, index=True, nullable=False)
     challenge_hash = Column(String(66), index=True)
     signage_point = Column(String(66), index=True)
     passed_filter = Column(Integer)
@@ -58,7 +59,7 @@ class FarmingInfoEvent(ChiaEvent):
 
 class PoolStateEvent(ChiaEvent):
     __tablename__ = "pool_state_events"
-    ts: float = Column(DateTime, primary_key=True)
+    ts = Column(DateTime, primary_key=True)
     current_points = Column(Integer)
     current_difficulty = Column(Integer)
     points_found_since_start = Column(Integer)
