a326f3dabfcfe076f5ae80cfe9adcec5dfa8b8e6
Daniel Semkowicz <daniel@semkowicz.pl>
2022-03-26 17:07:05 +0100

    Move configuration read function to configuration.py

--- monitor/__main__.py
+++ monitor/__main__.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 import logging
 import sys
 from asyncio.queues import Queue
@@ -10,14 +9,14 @@ from chia.util.config import load_config
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from sqlalchemy.exc import OperationalError
 
-from monitor.configuration import Configuration
+from monitor.configuration import Configuration, read_config
 from monitor.collectors.ws_collector import WsCollector
-from monitor.collectors.rpc_collector import RpcCollector, RpcCollectorConfiguration
-from monitor.collectors.price_collector import PriceCollector, PriceCollectorConfiguration
+from monitor.collectors.rpc_collector import RpcCollector
+from monitor.collectors.price_collector import PriceCollector
 from monitor.database import ChiaEvent, session
 from monitor.exporter import ChiaExporter
 from monitor.logger import ChiaLogger
-from monitor.notifier import Notifier, NotifierConfiguration
+from monitor.notifier import Notifier
 
 chia_config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
 
@@ -105,45 +104,12 @@ async def aggregator(exporter: ChiaExporter, notifier: Optional[Notifier], confi
         notifier.stop()
 
 
-def read_config():
-    with open("config.json") as f:
-        config = json.load(f)
-    return config
-
-
 if __name__ == "__main__":
     initilize_logging()
     try:
-        config_json = read_config()
-    except:
-        logging.error(
-            "Failed to read config.json. Please copy the config-example.json to config.json and configure it to your preferences."
-        )
-        sys.exit(1)
-
-    try:
-        config = Configuration(
-            config_json["exporter_port"],
-            RpcCollectorConfiguration(
-                config_json["rpc_collector"]["refresh_interval_seconds"]
-            ),
-            PriceCollectorConfiguration(
-                config_json["price_collector"]["refresh_interval_seconds"]
-            ),
-            NotifierConfiguration(
-                config_json["notifications"]["enable"],
-                config_json["notifications"]["refresh_interval_seconds"],
-                config_json["notifications"]["status_interval_minutes"],
-                config_json["notifications"]["lost_plots_alert_threshold"],
-                config_json["notifications"]["disable_proof_found_alert"],
-                config_json["notifications"]["status_service_url"],
-                config_json["notifications"]["alert_service_url"]
-            )
-        )
-    except KeyError as ex:
-        logging.error(
-            f"Failed to validate config. Missing required key {ex}. Please compare the fields of your config.json with the config-example.json and fix all inconsistencies."
-        )
+        config = read_config()
+    except RuntimeError as ex:
+        logging.error(ex)
         sys.exit(1)
 
     exporter = ChiaExporter(config.exporter_port)
